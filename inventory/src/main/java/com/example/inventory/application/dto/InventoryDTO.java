aab7ddf2da888f65484176e2205bec8612e10006
abhishekShaw-neu <85756053+abhishekShaw-neu@users.noreply.github.com>
2023-11-26 14:40:48 +0530

    commit 2

--- inventory/src/main/java/com/example/inventory/application/dto/InventoryDTO.java
+++ inventory/src/main/java/com/example/inventory/application/dto/InventoryDTO.java
@@ -1,4 +1,12 @@
 package com.example.inventory.application.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class InventoryDTO {
+    private int productId;
+    private int quantity;
+    // Other fields as needed
 }
